30d25491edeca5ac093628a6a7147034a4cf5dd7
西川和希 <slcin.567@gmail.com>
2022-02-10 20:42:23 +0900

    テーブルカラム修正

--- db/mysql/sql/person.sql
+++ db/mysql/sql/person.sql
@@ -6,16 +6,15 @@ DROP TABLE IF EXISTS person;
 
 CREATE TABLE person
 (
-    id INT AUTO_INCREMENT NOT NULL,
+    id VARCHAR(36) NOT NULL,
     name VARCHAR(255),
     birthday DATE,
-    age INT(3),
     email VARCHAR(255),
     PRIMARY KEY (id)
 );
 
-INSERT INTO person (name, birthday, age, email) VALUES ("新井 淳一", '1990-04-01', 31, 'arai@sample.com');
-INSERT INTO person (name, birthday, age, email) VALUES ("伊藤 広", '1991-06-23', 31, 'ito@sample.com');
-INSERT INTO person (name, birthday, age, email) VALUES ("宇都宮 隆", '1995-06-07', 27, 'utsunomiya@sample.com');
-INSERT INTO person (name, birthday, age, email) VALUES ("江本 裕美", '1990-11-12', 31, 'emoto@sample.com');
-INSERT INTO person (name, birthday, age, email) VALUES ("緒方 恵美", '1996-12-01', 25, 'ogata@sample.com');
+INSERT INTO person (id, name, birthday, email) VALUES (uuid(), "新井 淳一", '1990-04-01', 'arai@sample.com');
+INSERT INTO person (id, name, birthday, email) VALUES (uuid(),"伊藤 広", '1991-06-23', 'ito@sample.com');
+INSERT INTO person (id, name, birthday, email) VALUES (uuid(),"宇都宮 隆", '1995-06-07', 'utsunomiya@sample.com');
+INSERT INTO person (id, name, birthday, email) VALUES (uuid(),"江本 裕美", '1990-11-12', 'emoto@sample.com');
+INSERT INTO person (id, name, birthday, email) VALUES (uuid(),"緒方 恵美", '1996-12-01', 'ogata@sample.com');
